1df39f34872a78eee21acc03b247a70dd5626895
Ondrej Moravek <ondrej.moravek@dhl.com>
2022-11-16 10:23:54 +0100

    [DIS-18] service worker path and cache busting parameter

--- ui.apps/js/dev/Components/ServiceWorker.js
+++ ui.apps/js/dev/Components/ServiceWorker.js
@@ -8,7 +8,7 @@ class ServiceWorker {
   }
 
   register() {
-    navigator.serviceWorker.register('/etc.clientlibs/dhl/clientlibs/clientlib-site/resources/sw.js').then(() => {
+    navigator.serviceWorker.register('/discover/etc.clientlibs/dhl/clientlibs/clientlib-site/resources/sw.js?v=discoverDhl-20221115-1').then(() => {
       // console.log('ServiceWorker succesfully registered');
     }).catch(() => {
       // console.log('ServiceWorker registration failed: ', err);
