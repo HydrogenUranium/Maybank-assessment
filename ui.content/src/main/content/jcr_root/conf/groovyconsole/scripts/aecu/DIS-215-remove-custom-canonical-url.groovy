c0fcfc54c1ba6ba4372459e2742bf4eb3a74678a
grjesman <gregory.jesman@dhl.com>
2023-07-25 15:26:39 +0200

    DIS-215 Remove custom canonical url from page properties

---
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-215-remove-custom-canonical-url.groovy
@@ -0,0 +1,13 @@
+boolean DRY_RUN = true
+String propertyName = "cq:canonicalUrl"
+String pagesWithExistingPageProperty = "SELECT child.* FROM [nt:base] AS child INNER JOIN [cq:Page] AS parent ON ISCHILDNODE(child, parent) " +
+        "WHERE (ISDESCENDANTNODE(parent, '/content/dhl') AND NAME(child) = 'jcr:content') " +
+        "AND (child.[" + propertyName + "] IS NOT NULL OR child.[" + propertyName + "] <> '')"
+
+aecu.contentUpgradeBuilder()
+        .forResourcesBySql2Query(pagesWithExistingPageProperty)
+        .filterByHasProperty(propertyName)
+        .printProperty(propertyName)
+        .doDeleteProperty(propertyName)
+        .doActivateResource()
+        .run(DRY_RUN)
