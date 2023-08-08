474184aba4c2811e20f520bb936b45f8be5c2856
Dmytro Bratchun <Dmytro_Bratchun@epam.com>
2023-08-08 17:42:05 +0300

    DIS-248 add DIS-248-get-used-components-by-template-resource-type.groovy

---
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-248-get-used-components-by-template-resource-type.groovy
@@ -0,0 +1,35 @@
+import javax.jcr.query.Query
+
+def SCOPE = "/content/dhl/en-master"
+
+def TEMPLATE_RESOURCE_TYPE = "dhl/components/pages/article"
+
+def query = """
+        SELECT component.* FROM [cq:PageContent] AS articleContent
+        INNER JOIN [nt:unstructured] AS component ON ISDESCENDANTNODE(component, articleContent)
+        WHERE articleContent.[sling:resourceType] = '$TEMPLATE_RESOURCE_TYPE' 
+        AND isDescendantNode(articleContent, '$SCOPE')
+"""
+
+def executeQuery(query) {
+    session.workspace.queryManager.createQuery(query, Query.JCR_SQL2).execute()
+}
+
+def componentSet = executeQuery(query).nodes.collect(new HashSet(), {
+    if (it.hasProperty("sling:resourceType")) {
+        it.getProperty("sling:resourceType").getString()
+    }
+})
+
+componentSet.remove(null)
+
+componentSet.stream()
+        .map(path -> getResource(path))
+        .sorted((first, second) -> first.valueMap.get("componentGroup").compareTo(second.valueMap.get("componentGroup")))
+        .forEach(resource -> {
+            def valueMap = resource.valueMap
+            def group = valueMap.get('componentGroup')
+            def name = valueMap.get('jcr:title')
+            println("Group: $group, Name: $name, path: $resource.path")
+        })
+
