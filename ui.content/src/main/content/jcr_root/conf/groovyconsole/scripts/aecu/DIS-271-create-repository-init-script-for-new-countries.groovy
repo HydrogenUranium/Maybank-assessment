c1bb896dd6b21b012c07c9a3e3352f6d30f78089
Dmytro Bratchun <Dmytro_Bratchun@epam.com>
2023-08-15 09:11:39 +0300

    DIS-271 add new country groups

---
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-271-create-repository-init-script-for-new-countries.groovy
@@ -0,0 +1,39 @@
+def NEW_COUNTRIES_DATA = [
+        ["country": "United Kingdom",   "code": "gb"],
+        ["country": "Canada",	        "code": "ca"],
+        ["country": "Italy",	        "code": "it"],
+        ["country": "France",	        "code": "fr"],
+        ["country": "Korea",	        "code": "kr"],
+        ["country": "Thailand",	        "code": "th"],
+        ["country": "Belgium",	        "code": "be"],
+        ["country": "Ireland",	        "code": "ie"],
+        ["country": "Spain",	        "code": "es"],
+        ["country": "Pakistan",	        "code": "pk"],
+        ["country": "Taiwan",	        "code": "tw"],
+        ["country": "Switzerland",	    "code": "ch"],
+        ["country": "Czech Republic",	"code": "cz"],
+        ["country": "Vietnam",	        "code": "vn"],
+        ["country": "China",	        "code": "cn"],
+        ["country": "Poland",	        "code": "pl"],
+        ["country": "Bangladesh",	    "code": "bd"],
+        ["country": "Denmark",	        "code": "dk"]
+]
+
+NEW_COUNTRIES_DATA.each {
+    def id = it.country.toLowerCase().replaceAll(" ", "-")
+    def country = it.country
+    def code = it.code
+    println("""
+        "create group author-$id",
+        "add author-$id to group discover-base-user",
+        "set properties on authorizable(author-$id)/profile\\n set givenName to \\"Author - $country\\"\\n end",
+        "set ACL for author-$id\\nallow jcr:read,jcr:versionManagement,rep:write,jcr:lockManagement on /content/dhl restriction(rep:glob,/$code)\\nend",
+        "set ACL for author-$id\\nallow jcr:read on home(author-$id)\\nend",
+
+        "create group publisher-$id",
+        "add publisher-$id to group discover-base-user",
+        "set properties on authorizable(publisher-$id)/profile\\n set givenName to \\"Publisher - $country\\"\\n end",
+        "set ACL for publisher-$id\\nallow jcr:read,jcr:versionManagement,crx:replicate,rep:write,jcr:lockManagement on /content/dhl restriction(rep:glob,/$code)\\nend",
+        "set ACL for publisher-$id\\nallow jcr:read on home(publisher-$id)\\nend",
+    """)
+}
