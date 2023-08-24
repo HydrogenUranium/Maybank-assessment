ff2c0f5c0860d74a8bb4ac638260c9a65f2982ea
Ondrej Moravek <ondrej.moravek@dhl.com>
2023-08-24 14:09:42 +0200

    [DIS-289] Fix the bugs

--- splunk-proxy/event-collector-stage/__init__.py
+++ splunk-proxy/event-collector-stage/__init__.py
@@ -2,7 +2,6 @@
 
 import logging
 import azure.functions as func
-import json
 import os
 from lib.processing import process_request
 
@@ -12,7 +11,9 @@ VALID_TOKENS = [os.environ['TOKEN_STAGE']]
 def main(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
 
     logging.info('Message received by Azure Functions')
-    req_body_string = process_request(req, VALID_TOKENS)
-    msg.set(req_body_string)
-    logging.info('Message is submitted to eventHub')
-    return func.HttpResponse(json.dumps({'text': 'Success', 'code': 0}), status_code=200)
+    http_response = process_request(req, VALID_TOKENS)
+    if http_response.status_code == 200:
+        req_body_string = req.get_body().decode("utf-8")
+        msg.set(req_body_string)
+        logging.info('Message is submitted to eventHub')
+    return http_response
