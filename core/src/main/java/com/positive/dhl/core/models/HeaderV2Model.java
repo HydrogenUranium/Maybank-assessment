07d9119536f306ae19c505a37bd9bca083644f48
grjesman <gregory.jesman@dhl.com>
2023-09-08 13:59:51 +0200

    DIS-231 New Header

--- core/src/main/java/com/positive/dhl/core/models/HeaderV2Model.java
+++ core/src/main/java/com/positive/dhl/core/models/HeaderV2Model.java
@@ -4,14 +4,12 @@ import com.day.cq.wcm.api.Page;
 import com.positive.dhl.core.injectors.InjectHomeProperty;
 import com.positive.dhl.core.services.PageUtilService;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.LinkedList;
@@ -27,45 +25,28 @@ public class HeaderV2Model {
     private Page currentPage;
 
     @InjectHomeProperty
+    @Getter
+    @Named("header/buttonName")
+    private String buttonName;
+
+    @InjectHomeProperty
+    @Getter
+    @Named("header/buttonLink")
+    private String buttonLink;
+
+    @InjectHomeProperty
+    @Named("header/companyLinks")
     private Resource companyLinks;
-
-    @InjectHomeProperty
-    @Named("header-typeOpenBusinessAccountButton")
-    private String typeOpenBusinessAccountButton;
-
-    @InjectHomeProperty
-    @Getter
-    @Named("header-openBusinessAccountButtonName")
-    private String openBusinessAccountButtonName;
-
-    @InjectHomeProperty
-    @Getter
-    @Named("header-openBusinessAccountButtonLink")
-    private String openBusinessAccountButtonLink;
-
-    @InjectHomeProperty
-    @Named("ctaBannerOpenBusinessAccount-buttonName")
-    private String openBusinessAccountButtonNameFromBanner;
-
-    @InjectHomeProperty
-    @Named("ctaBannerOpenBusinessAccount-buttonLink")
-    private String openBusinessAccountButtonLinkFromBanner;
 
     public List<CompanyLink> getCompanyLinks() {
         List<CompanyLink> links = new LinkedList<>();
-        companyLinks.listChildren().forEachRemaining(r -> links.add(r.adaptTo(CompanyLink.class)));
+        if (companyLinks != null) {
+            companyLinks.listChildren().forEachRemaining(r -> links.add(r.adaptTo(CompanyLink.class)));
+        }
         return links;
     }
 
     public String getHomePageLink() {
         return pageUtilService.getHomePage(currentPage).getPath() + ".html";
-    }
-
-    @PostConstruct
-    protected void init() {
-        if (!StringUtils.equals(typeOpenBusinessAccountButton, "custom")) {
-            openBusinessAccountButtonName = openBusinessAccountButtonNameFromBanner;
-            openBusinessAccountButtonLink = openBusinessAccountButtonLinkFromBanner;
-        }
     }
 }
---
+++ core/src/test/java/com/positive/dhl/core/models/HeaderV2ModelTest.java
@@ -0,0 +1,74 @@
+package com.positive.dhl.core.models;
+
+import com.day.cq.wcm.api.Page;
+import com.positive.dhl.core.injectors.AssetInjector;
+import com.positive.dhl.core.injectors.HomePropertyInjector;
+import com.positive.dhl.core.services.PageUtilService;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.spi.Injector;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith({AemContextExtension.class, MockitoExtension.class})
+class HeaderV2ModelTest {
+    private final static String COMPONENT_LOCATION = "/content/home/small-business-advice/article/jcr:content/root/header";
+
+    private final AemContext context = new AemContext(ResourceResolverType.JCR_MOCK);
+
+    @Mock
+    private PageUtilService pageUtils;
+
+    @InjectMocks
+    private HomePropertyInjector homePropertyInjector;
+
+    @InjectMocks
+    private AssetInjector assetInjector;
+
+    @BeforeEach
+    void setUp() {
+        context.registerService(Injector.class, assetInjector);
+        context.registerService(Injector.class, homePropertyInjector);
+        context.registerService(PageUtilService.class, pageUtils);
+        context.addModelsForClasses(CtaBanner.class);
+    }
+
+    @Test
+    void test_withProperties() {
+        context.load().json("/com/positive/dhl/core/models/HeaderV2/withProperties.json", "/content");
+        when(pageUtils.getHomePage(any())).thenReturn(context.resourceResolver().getResource("/content/home").adaptTo(Page.class));
+
+        Resource resource = context.resourceResolver().getResource(COMPONENT_LOCATION);
+        HeaderV2Model headerV2Model = resource.adaptTo(HeaderV2Model.class);
+        assertEquals("Button Name", headerV2Model.getButtonName());
+        assertEquals("/content/dhl/button", headerV2Model.getButtonLink());
+        assertEquals("/content/home.html", headerV2Model.getHomePageLink());
+        assertEquals("Link Name 1", headerV2Model.getCompanyLinks().get(0).getLinkName());
+        assertEquals("/content/dhl/link/1", headerV2Model.getCompanyLinks().get(0).getLinkPath());
+    }
+
+    @Test
+    void test_withoutProperties() {
+        context.load().json("/com/positive/dhl/core/models/HeaderV2/withoutProperties.json", "/content");
+        when(pageUtils.getHomePage(any())).thenReturn(context.resourceResolver().getResource("/content/home").adaptTo(Page.class));
+
+        Resource resource = context.resourceResolver().getResource(COMPONENT_LOCATION);
+        HeaderV2Model headerV2Model = resource.adaptTo(HeaderV2Model.class);
+        assertNull(headerV2Model.getButtonName());
+        assertNull(headerV2Model.getButtonLink());
+        assertEquals("/content/home.html", headerV2Model.getHomePageLink());
+        assertEquals(Collections.emptyList(), headerV2Model.getCompanyLinks());
+    }
+}
