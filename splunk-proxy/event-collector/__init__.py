69297449bb4a881ea9c1f2a53b8bea66eb7a1847
Ondrej Moravek <ondrej.moravek@dhl.com>
2023-09-15 12:21:19 +0200

    [DIS-289] Simplify the proxy setup and route change

--- splunk-proxy/event-collector/__init__.py
+++ splunk-proxy/event-collector/__init__.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 
 import logging
-import json
 import azure.functions as func
+import json
+import os
+import typing
+
+TOKEN_NAME_PROD = "TOKEN_PROD"
+TOKEN_NAME_STAGE = "TOKEN_STAGE"
 
 
-def process_request(req: func.HttpRequest, valid_tokens: list[str]) -> func.HttpResponse:
+def store_to_eventhub(msgprod: func.Out[typing.List[str]], msgstage: func.Out[typing.List[str]], body: str):
+    output_prod = []
+    output_stage = []
+    req_body_list = body.replace('}{', '}|||{').split('|||')
+    for event in req_body_list:
+        data_dict = json.loads(event)
+        if data_dict['index'] == "discover_prod":
+            output_prod.append(event)
+        if data_dict['index'] == "discover_staging":
+            output_stage.append(event)
+    if len(output_prod) > 0:
+        msgprod.set(output_prod)
+    if len(output_stage) > 0:
+        msgstage.set(output_stage)
+
+
+def main(req: func.HttpRequest, msgprod: func.Out[typing.List[str]], msgstage: func.Out[typing.List[str]]) -> func.HttpResponse:
+
+    try:
+        VALID_TOKENS = [os.environ[TOKEN_NAME_PROD],
+                        os.environ[TOKEN_NAME_STAGE]]
+    except KeyError as e:
+        return func.HttpResponse(f"Environment variable is not set ${e}", status_code=500)
+
+    logging.info('Message received by Azure Functions')
 
     json_mimetype: str = "application/json"
     req_body_bytes: bytes
@@ -38,7 +67,7 @@ def process_request(req: func.HttpRequest, valid_tokens: list[str]) -> func.Http
             json.dumps({'text': 'Invalid authorization', 'code': 3}),
             status_code=401, mimetype=json_mimetype)
 
-    if auth_token is None or auth_token not in valid_tokens:
+    if auth_token is None or auth_token not in VALID_TOKENS:
         logging.warn('Invalid token')
         return func.HttpResponse(
             json.dumps({'text': 'Invalid token', 'code': 4}),
@@ -51,4 +80,9 @@ def process_request(req: func.HttpRequest, valid_tokens: list[str]) -> func.Http
         return func.HttpResponse(
             json.dumps({'text': 'Payload is too large', 'code': 99}), status_code=400, mimetype=json_mimetype)
 
+    req_body_string = req.get_body().decode("utf-8")
+    logging.info(f"Message: {req_body_string}")
+    store_to_eventhub(msgprod, msgstage, req_body_string)
+
+    logging.info('Message is submitted to eventHub')
     return func.HttpResponse(json.dumps({'text': 'Success', 'code': 0}), status_code=200, mimetype=json_mimetype)
