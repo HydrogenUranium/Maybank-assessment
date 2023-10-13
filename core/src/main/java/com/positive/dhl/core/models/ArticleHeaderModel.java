01f850e22954db41a43771ae9666834a2312cbf6
grjesman <gregory.jesman@dhl.com>
2023-10-13 13:03:24 +0200

    DIS-291 Article Page: Main Layout

--- core/src/main/java/com/positive/dhl/core/models/ArticleHeaderModel.java
+++ core/src/main/java/com/positive/dhl/core/models/ArticleHeaderModel.java
@@ -1,44 +1,96 @@
 package com.positive.dhl.core.models;
 
+import com.day.cq.wcm.api.Page;
+import com.positive.dhl.core.injectors.InjectHomeProperty;
 import lombok.Getter;
+import lombok.NonNull;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@Model(adaptables= Resource.class, defaultInjectionStrategy= DefaultInjectionStrategy.OPTIONAL)
+import static com.day.cq.wcm.api.constants.NameConstants.*;
+
+
+@Model(adaptables=SlingHttpServletRequest.class, defaultInjectionStrategy= DefaultInjectionStrategy.OPTIONAL)
 public class ArticleHeaderModel {
+    @Inject
+    private Page currentPage;
+
     @Getter
-    @ValueMapValue
     private String articleTitle;
 
     @Getter
-    @ValueMapValue
     private String publishDate;
 
     @Getter
-    @ValueMapValue
+    private String publishDateFriendly;
+
+    @Getter
     private String readingDuration;
 
+    @InjectHomeProperty
     @Getter
-    @ValueMapValue
+    @Named("articleHeader-shareOn")
     private String shareOn;
 
+    @InjectHomeProperty
     @Getter
-    @ValueMapValue
+    @Named("articleHeader-smartShareButtonsLabel")
     private String smartShareButtonsLabel;
 
+    @InjectHomeProperty
     @Getter
-    @ValueMapValue
+    @Named("articleHeader-smartShareButtonsIconPath")
     private String smartShareButtonsIconPath;
 
-    @Inject
+    @InjectHomeProperty
+    @Getter
+    @Named("articleHeader-followLabel")
+    private String followLabel;
+
+    @InjectHomeProperty
+    @Getter
+    @Named("articleHeader-followPath")
+    private String followPath;
+
+    @InjectHomeProperty
+    @Named("multifields/socialNetwork")
     private Resource socialNetwork;
+
+    @PostConstruct
+    protected void init() {
+        ValueMap currentPageProperties = currentPage.getProperties();
+        if (currentPageProperties != null ) {
+            articleTitle = currentPageProperties.get(PN_TITLE, String.class);
+
+            Date publishDateValue = getPublishDate(currentPageProperties);
+            publishDate = new SimpleDateFormat("yyyy-MM-dd").format(publishDateValue);
+            publishDateFriendly = new SimpleDateFormat("dd MMMM yyyy").format(publishDateValue);
+
+            readingDuration = currentPageProperties.get("readtime", String.class);
+        }
+    }
+
+    private Date getPublishDate(@NonNull ValueMap properties) {
+        Date customPublishDate = properties.get("custompublishdate", Date.class);
+        if (customPublishDate != null) {
+            return customPublishDate;
+        } else {
+            Date jcrCreated = properties.get(PN_CREATED, new Date());
+            Date cqLastModified = properties.get(PN_PAGE_LAST_MOD, new Date());
+            return jcrCreated.after(cqLastModified) ? jcrCreated : cqLastModified;
+        }
+    }
 
     public Map<String, String> getSocialNetwork() {
         Map<String, String> socialNetworkMap = new LinkedHashMap<>();
