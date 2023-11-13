38e90ed775d483339c7ec64dc5365515763ad6b0
grjesman <gregory.jesman@dhl.com>
2023-11-13 17:31:40 +0100

    DIS-472 BackUp Groovy Script

---
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-472-content-backup-release8.groovy
@@ -0,0 +1,53 @@
+/*
+DIS-472 BackUp before content manipulation
+
+Steps:
+1)  def DRY_RUN = true
+    - create a list of filters to prepare a BackUp package with all pages that will be affected
+
+2)  def DRY_RUN = false
+    - update the version of each page that will be affected
+*/
+import java.text.SimpleDateFormat;
+
+def DRY_RUN = true
+
+def VERSION_NAME = "DIS-472 Before modifying on STAGE"
+
+// Section for preparing list of pages
+def contentPath = "/content/dhl"
+def resourceType = "dhl/components/pages/standard"
+def getPagesByResourceType = """
+            SELECT * FROM [cq:Page] AS page
+            WHERE ISDESCENDANTNODE(page, '$contentPath')
+            AND page.[jcr:content/sling:resourceType] = '$resourceType'
+            AND NAME(page) LIKE '%thank%'
+        """
+def getPagesByQuery = session.getWorkspace().getQueryManager().createQuery(getPagesByResourceType, 'JCR-SQL2')
+listPages = getPagesByQuery
+        .execute()
+        .getNodes()
+        .collect{it.getPath()}
+        .sort()
+//END
+
+if (DRY_RUN) {
+    println(">> DRY RUN mode")
+    println("Results: " + listPages.size())
+    if (listPages.size() > 0) {
+        println("(!) Use this list of pages for preparing package:")
+        listPages.each({ println("""<filter root="$it"/>""")})
+    }
+} else {
+    println("Results: " + listPages.size())
+    println("List of pages whose version was updated:")
+    listPages.each({
+        println(it)
+        def pageManager = resourceResolver.adaptTo(PageManager.class);
+        def page = getPage(it);
+        def date = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date());
+        def label = String.format("%s - %s", VERSION_NAME, date);
+
+        pageManager.createRevision(page, label, "Groovy Script version");
+    })
+}
