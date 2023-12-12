74e7edcb77e684c7a3259ef7f7b191c1ec951494
grjesman <gregory.jesman@dhl.com>
2023-12-12 03:06:45 +0100

    DIS-525 Added Article NULL check

--- core/src/main/java/com/positive/dhl/core/models/PageNotFound.java
+++ core/src/main/java/com/positive/dhl/core/models/PageNotFound.java
@@ -10,6 +10,7 @@ import javax.jcr.Session;
 import com.positive.dhl.core.services.PageUtilService;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -17,10 +18,8 @@ import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
 
 import com.day.cq.search.PredicateGroup;
-import com.day.cq.search.Query;
 import com.day.cq.search.QueryBuilder;
 import com.day.cq.search.result.Hit;
-import com.day.cq.search.result.SearchResult;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 
 import static com.day.cq.wcm.api.constants.NameConstants.NT_PAGE;
@@ -48,65 +47,78 @@ public class PageNotFound {
 	@PostConstruct
     protected void init() throws RepositoryException {
         if (builder != null) {
-			//get search-results
-			Map<String, String> map = new HashMap<>();
-			map.put("type", NT_PAGE);
-			map.put("property", "jcr:content/sling:resourceType");
-			map.put("property.value", "dhl/components/pages/searchresults");
-			Query query = builder.createQuery(PredicateGroup.create(map), resourceResolver.adaptTo(Session.class));
-		    SearchResult searchResult = query.getResult();
-		    if (searchResult != null) {
-				searchResultsPage = searchResult.getHits().get(0).getPath().concat(".html");
-
-				Iterator<Resource> resources = searchResult.getResources();
-				if (resources.hasNext()) {
-					resources.next().getResourceResolver().close();
-				}
-		    }
+			searchResultsPage = receiveSearchResultsPage();
+			trendingArticles = receiveTrendingArticles();
         }
-        
-		trendingArticles = new ArrayList<>();
-		if (builder != null) {
-			Map<String, String> map = new HashMap<>();
-			map.put("type", NT_PAGE);
+	}
 
-			map.put("group.p.or", "true");
-			
-			List<String> articleTypes = Article.getArticlePageTypes();
-			for (int x = 0; x < articleTypes.size(); x++) {
-				map.put(String.format("group.%1$s_property", (x + 1)), "jcr:content/sling:resourceType");
-				map.put(String.format("group.%1$s_property.value", (x + 1)), String.format("dhl/components/pages/%1$s", articleTypes.get(x)));
-				map.put(String.format("group.%1$s_property.operation", (x + 1)), "like");
+	private String receiveSearchResultsPage() throws RepositoryException {
+		String result = StringUtils.EMPTY;
+		Map<String, String> map = new HashMap<>();
+		map.put("type", NT_PAGE);
+		map.put("property", "jcr:content/sling:resourceType");
+		map.put("property.value", "dhl/components/pages/searchresults");
+		var query = builder.createQuery(PredicateGroup.create(map), resourceResolver.adaptTo(Session.class));
+		var searchResult = query.getResult();
+		if (searchResult != null) {
+			result = searchResult.getHits().get(0).getPath().concat(".html");
+
+			Iterator<Resource> resources = searchResult.getResources();
+			if (resources.hasNext()) {
+				resources.next().getResourceResolver().close();
 			}
-			
-			map.put("orderby", "@jcr:content/custompublishdate");
-			map.put("orderby.sort", "desc");
-			
-			map.put("p.limit", "3");
-
-			Query query = builder.createQuery(PredicateGroup.create(map), resourceResolver.adaptTo(Session.class));
-	        SearchResult searchResult = query.getResult();
-	        if (searchResult != null) {
-				int count = 0;
-				for (Hit hit: searchResult.getHits()) {
-					ValueMap hitProperties = hit.getProperties();
-					boolean hideInNav = hitProperties.get(PN_HIDE_IN_NAV, false);
-					if (!hideInNav) {
-						var article = pageUtilService.getArticle(hit.getPath(), resourceResolver);
-						if (article != null) {
-							article.setIndex(count);
-							trendingArticles.add(article);
-							count++;
-						}
-						if (count > 3) break;
-					}
-				}
-
-				Iterator<Resource> resources = searchResult.getResources();
-				if (resources.hasNext()) {
-					resources.next().getResourceResolver().close();
-				}
-	        }
 		}
+
+		return result;
+	}
+
+	private List<Article> receiveTrendingArticles() throws RepositoryException {
+		List<Article> result = new ArrayList<>();
+		var query = builder.createQuery(PredicateGroup.create(getArticlesPageTypeQueryMap()), resourceResolver.adaptTo(Session.class));
+		var searchResult = query.getResult();
+		if (searchResult != null) {
+			var count = 0;
+			for (Hit hit: searchResult.getHits()) {
+				var article = getArticle(hit);
+				if (article != null) {
+					article.setIndex(count);
+					result.add(article);
+					count++;
+				}
+				if (count > 3) break;
+			}
+
+			Iterator<Resource> resources = searchResult.getResources();
+			if (resources.hasNext()) {
+				resources.next().getResourceResolver().close();
+			}
+		}
+		return result;
+	}
+
+	private Article getArticle(Hit hit) throws RepositoryException {
+		ValueMap hitProperties = hit.getProperties();
+		boolean hideInNav = hitProperties.get(PN_HIDE_IN_NAV, false);
+		return !hideInNav ? pageUtilService.getArticle(hit.getPath(), resourceResolver) : null;
+	}
+
+	private Map<String, String> getArticlesPageTypeQueryMap() {
+		Map<String, String> map = new HashMap<>();
+		map.put("type", NT_PAGE);
+
+		map.put("group.p.or", "true");
+
+		List<String> articleTypes = Article.getArticlePageTypes();
+		for (var x = 0; x < articleTypes.size(); x++) {
+			map.put(String.format("group.%1$s_property", (x + 1)), "jcr:content/sling:resourceType");
+			map.put(String.format("group.%1$s_property.value", (x + 1)), String.format("dhl/components/pages/%1$s", articleTypes.get(x)));
+			map.put(String.format("group.%1$s_property.operation", (x + 1)), "like");
+		}
+
+		map.put("orderby", "@jcr:content/custompublishdate");
+		map.put("orderby.sort", "desc");
+
+		map.put("p.limit", "3");
+		return map;
 	}
 }
