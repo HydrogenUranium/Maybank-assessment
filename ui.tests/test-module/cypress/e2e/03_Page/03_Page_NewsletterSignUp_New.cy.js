6f60365c62ceea71c545753d4010442250a6eadb
grjesman <gregory.jesman@dhl.com>
2024-01-09 13:13:46 +0100

    DIS-22 First custom UI Test (code formatting)

--- ui.tests/test-module/cypress/e2e/03_Page/03_Page_NewsletterSignUp_New.cy.js
+++ ui.tests/test-module/cypress/e2e/03_Page/03_Page_NewsletterSignUp_New.cy.js
@@ -1,56 +1,40 @@
 describe('Subscribe newsletter page', () => {
 
-const pageUrl = Cypress.env('AEM_PUBLISH_URL') + '/content/dhl/global/en-global/newsletter-sign-up.html';
+    const pageUrl = Cypress.env('AEM_PUBLISH_URL') + '/content/dhl/global/en-global/newsletter-sign-up.html';
 
-beforeEach(() => {
-    cy.visit(pageUrl);
-    cy.get("button#onetrust-accept-btn-handler").contains("Accept All").click();
-  });
-
-
-  it('Verify the title contains the correct text "Subscribe for the latest insights"', () => {
-
-    //cy.get('#onetrust-accept-btn-handler').click();
-      cy.get('.cmp-title__text').contains('Subscribe for the latest insights')
-      cy.wait (2000);
-      //cy.get(data.AcceptAll_cookies).click();
-  });
-
-
-  it('Verify if all fields are present', () => {
-      cy.get('.cmp-title__text').should('exist');
-      cy.get('.landing-points-component').should('exist');
-      cy.get('#Email').should('exist');
-      cy.get('#suspectCountry').should('exist');
-      cy.get('.mktoButton').should('exist');
-  });
-
-
-   it('Verify the form submits successfully when all fields are filled out correctly', () => {
-
-       // Fill in the form fields
-       cy.get('#Email').type('test@gmail.com');
-       cy.get('#suspectCountry').select('Afghanistan');
-       // Submit the form
-       cy.get('.mktoButton').click();
-   });
-
-
-    it('Verify that an error message "This field is required" is displayed when a required field is left blank', () => {
-       cy.get('#Email').type('test@gmail.com');
-       cy.get('.mktoButton').click();
-       cy.get('#ValidMsgsuspectCountry').should('be.visible').and('contain', 'This field is required.') ;
-
+    beforeEach(() => {
+        cy.visit(pageUrl);
+        cy.get("button#onetrust-accept-btn-handler").contains("Accept All").click();
     });
 
-   it('Verify an error message is displayed for each field when the input is invalid (e.g., an email field without an @', () => {
+    it('Verify the title contains the correct text "Subscribe for the latest insights"', () => {
+        cy.get('.cmp-title__text').contains('Subscribe for the latest insights')
+        cy.wait (2000);
+    });
+
+    it('Verify if all fields are present', () => {
+        cy.get('.cmp-title__text').should('exist');
+        cy.get('.landing-points-component').should('exist');
+        cy.get('#Email').should('exist');
+        cy.get('#suspectCountry').should('exist');
+        cy.get('.mktoButton').should('exist');
+    });
+
+    it('Verify the form submits successfully when all fields are filled out correctly', () => {
+        cy.get('#Email').type('test@gmail.com');
+        cy.get('#suspectCountry').select('Afghanistan');
+        cy.get('.mktoButton').click();
+    });
+
+    it('Verify that an error message "This field is required" is displayed when a required field is left blank', () => {
+        cy.get('#Email').type('test@gmail.com');
+        cy.get('.mk  toButton').click();
+        cy.get('#ValidMsgsuspectCountry').should('be.visible').and('contain', 'This field is required.') ;
+    });
+
+    it('Verify an error message is displayed for each field when the input is invalid (e.g., an email field without an @', () => {
         cy.get('#Email').type('test');
         cy.get('.mktoButton').click();
         cy.get('#ValidMsgEmail.mktoErrorMsg').should('be.visible').and('contain', 'Must be valid email.','example@yourdomain.com') ;
     });
-
-
-
-
-
-   });
+});
