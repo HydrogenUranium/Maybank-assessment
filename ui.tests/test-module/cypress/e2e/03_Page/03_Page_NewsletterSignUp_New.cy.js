64abc5290dc046226215190924d160ea2e2ba60a
grjesman <gregory.jesman@dhl.com>
2024-01-12 17:01:56 +0100

    DIS-22 Fixed Content sensitive test

--- ui.tests/test-module/cypress/e2e/03_Page/03_Page_NewsletterSignUp_New.cy.js
+++ ui.tests/test-module/cypress/e2e/03_Page/03_Page_NewsletterSignUp_New.cy.js
@@ -1,6 +1,6 @@
 describe('Subscribe newsletter page', () => {
 
-    const pageUrl = Cypress.env('AEM_PUBLISH_URL') + '/content/dhl/global/en-global/newsletter-sign-up.html';
+    const pageUrl = 'https://publish-p58772-e1126664.adobeaemcloud.com' + '/content/dhl/global/en-global/newsletter-sign-up.html';
 
     beforeEach(() => {
         cy.visit(pageUrl);
@@ -8,7 +8,7 @@ describe('Subscribe newsletter page', () => {
     });
 
     it('Verify the title contains the correct text "Subscribe for the latest insights"', () => {
-        cy.get('.cmp-title__text').should('exist')
+        cy.get('#title-v2-9ab11e8541').should('exist')
         cy.wait (2000);
     });
 
