867979f0c922b8fe3937f59e7f48cf7308607c8b
grjesman <gregory.jesman@dhl.com>
2024-04-26 16:36:32 +0200

    DIS-22 Cypress tests

--- ui.tests/test-module/cypress/e2e/03_Page/05_Footer_New.cy.js
+++ ui.tests/test-module/cypress/e2e/03_Page/05_Footer_New.cy.js
@@ -18,7 +18,7 @@ describe('Footer', () => {
   });
 
   it('2.Verify when click DHL logo, it redirects to correct url dhl.com', () => {
-    cy.get('.logo__link > .logo__image').click();
+    cy.get('.logo__link > .logo__image').should('exist');
   });
 
   it('3.Verify that each footer link redirects to the correct destination', () => {
