f0f33a858e7de4ed23b67cae070ac00880d0206a
grjesman <gregory.jesman@dhl.com>
2024-05-06 17:03:20 +0200

    DIS-496 Enabled Carousel Indicator on Touch Devices

--- ui.apps/js/dev/Components/Carousel.js
+++ ui.apps/js/dev/Components/Carousel.js
@@ -3,6 +3,8 @@ class Carousel {
     this.sel = {
       component: '.article-carousel',
       indicator: '.cmp-carousel__indicator',
+      previous: '.cmp-carousel__action.cmp-carousel__action--previous',
+      next: '.cmp-carousel__action.cmp-carousel__action--next',
     };
     this.carousel = null;
 
@@ -11,13 +13,30 @@ class Carousel {
   }
 
   bindEvents() {
-    $(this.sel.indicator).each(function( index ) {
-      if ('ontouchstart' in window) {
+    if ('ontouchstart' in window) {
+      $(this.sel.indicator).each(function() {
         this.addEventListener("touchstart", function() {
-          this.trigger('click');
+          $(this).trigger('click');
         }, false);
-      }
-    });
+      });
+
+      var tStart = null;
+      $(document).bind('touchstart', function (e) {
+        tStart = (evt.touches || evt.originalEvent.touches)[0].clientX;
+      });
+
+      $(document).bind('touchend', function (e) {
+        if (!tStart) {
+          return;
+        }
+        var tEnd = e.originalEvent.changedTouches[0].clientX;
+        if (tStart - tEnd > 5) {
+          $(this.sel.previous).trigger('click');
+        } else if (tEnd - tStart > 5) {
+          $(this.sel.next).trigger('click');
+        }
+      }, this);
+    }
   }
 
   init() {
