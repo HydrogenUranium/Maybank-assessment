a55627478487a912aeabdd517441ba49d09e2373
Dmytro Bratchun <Dmytro_Bratchun@epam.com>
2024-05-24 10:53:40 +0300

    feature/republish-list-of-pages

---
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/content-management/republish-list-of-pages.groovy
@@ -0,0 +1,42 @@
+def DRY_RUN = true
+
+def LIST = [
+// '/content/dhl/hk/en-hk/example-landing-page/jcr:content',
+]
+
+def wrongPaths = []
+def notPublished = []
+
+def getJcrContent(path) {
+    return pageManager.getContainingPage(path).getContentResource();
+}
+
+def isPublished(path) {
+    def resource = getJcrContent(path)
+    def valueMap = resource.getValueMap()
+    def status = valueMap.get('cq:lastReplicationAction_publish', valueMap.get('cq:lastReplicationAction', ''))
+    return status.equals('Activate')
+}
+
+def filtered = LIST.stream()
+        .filter(path -> {
+            if(getResource(path) == null) {
+                wrongPaths.add(path)
+                return false
+            }
+            if(!isPublished(path)) {
+                notPublished.add(path)
+                return false;
+            }
+            return true
+        }).toList()
+
+println("Wrong paths: $wrongPaths.size")
+println("Not published: $notPublished.size")
+println("Pages to publish: $filtered.size")
+
+filtered.each({
+    aecu.contentUpgradeBuilder().forResources((String[])[it])
+            .doActivateResource()
+            .run(DRY_RUN)
+})
