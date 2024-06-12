42c5077e0fd913445b5f94ea5fbbd3682566afc3
grjesman <gregory.jesman@dhl.com>
2024-06-13 01:41:59 +0200

    DIS-716 Removing empty HTML5 Embed components

---
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-716-remove-empty-html5-components.groovy
@@ -0,0 +1,235 @@
+/* DIS-716 Removing empty HTML5 Embed components
+Steps:
+
+0)  INITIAL SETUP
+    - define scope
+
+1) AFFECTED ITEMS:
+    - show a list of affected items
+    - add affected items to the '@Field affectedItemPaths'
+    @Field dryRun = true
+    @Field contentManipulation = false
+
+2) BACKUP:
+    - create a backup package of the pages that will be affected
+    - update the version of each page that will be affected
+    @Field dryRun = false
+    @Field contentManipulation = false
+
+3)  MANIPULATION:
+    @Field dryRun = false
+    @Field contentManipulation = true
+
+4)  CHECK:
+    - remove items from '@Field affectedItemPaths'
+    - check result (expected: 'Results: 0')
+    @Field dryRun = true
+    @Field contentManipulation = false
+
+*/
+
+import groovy.transform.Field
+import java.text.SimpleDateFormat;
+
+@Field dryRun = true
+@Field contentManipulation = false
+@Field contentScope = "/content/dhl"
+
+@Field affectedItemPaths = [
+]
+
+@Field versionAndPackageName = "DIS-716-before-removing-empty-html5-component"
+
+@Field packagesPath = "/etc/packages/my_packages"
+@Field packageDefinitionPath = "$packagesPath/${versionAndPackageName}.zip/jcr:content/vlt:definition"
+
+main()
+
+/* Methods */
+
+// main
+def main() {
+    affectedItemPaths = getAffectedItemPaths()
+    if (contentManipulation) {
+        def affectedPagePaths = getAffectedPagePaths(affectedItemPaths)
+        removingHtml5Components(affectedItemPaths)
+        publishingAffectedPages(affectedPagePaths)
+    } else {
+        if (dryRun) {
+            showAffectedItems(affectedItemPaths)
+        } else {
+            def affectedPagePaths = getAffectedPagePaths(affectedItemPaths)
+            createBackupPackage(affectedPagePaths)
+            setNewPageVersion(affectedPagePaths)
+        }
+    }
+}
+
+def getAffectedItemPaths() {
+    def paths = []
+    if (affectedItemPaths.size() == 0) {
+        sql2Query("""
+            SELECT * FROM [nt:unstructured] AS node 
+            WHERE ISDESCENDANTNODE([$contentScope]) 
+            AND (node.[sling:resourceType]="dhl/components/content/html5embed") 
+            AND (node.[text] IS NULL OR node.[text] = '')
+            """).each { paths.add(it.path) }
+
+        return paths.sort().findAll{!it.startsWith("/content/dhl/Archive")}
+    } else {
+        return affectedItemPaths
+    }
+}
+
+def removingHtml5Components(affectedNodePaths) {
+    affectedNodePaths.each { nodePath ->
+        aecu.contentUpgradeBuilder()
+                .forResources((String[]) [nodePath])
+                .doDeleteResource()
+                .run(dryRun)
+    }
+}
+
+def publishingAffectedPages(affectedPagePaths) {
+    affectedPagePaths.each { pagePath ->
+        aecu.contentUpgradeBuilder()
+                .forResources((String[]) [pagePath + "/jcr:content"])
+                .filterByProperty("cq:lastReplicationAction", "Activate")
+                .doActivateContainingPage()
+                .run(dryRun)
+        aecu.contentUpgradeBuilder()
+                .forResources((String[]) [pagePath + "/jcr:content"])
+                .filterByProperty("cq:lastReplicationAction", "Deactivate")
+                .doDeactivateContainingPage()
+                .run(dryRun)
+    }
+}
+
+def showAffectedItems(affectedItemPaths) {
+    println("Affected items: " + affectedItemPaths.size())
+    if (affectedItemPaths.size() > 0) {
+        affectedItemPaths.each({ println(""""$it",""") })
+    }
+}
+
+def getAffectedPagePaths(affectedItemPaths) {
+    def listPages = []
+    def pageUtilService = getService("com.positive.dhl.core.services.PageUtilService")
+
+    affectedItemPaths.each { nodePath ->
+        listPages.add(pageUtilService.getPage(getResource(nodePath)).path)
+    }
+
+    return listPages.unique();
+}
+
+def setNewPageVersion(listPages) {
+    println("----------------------------------------")
+    if (dryRun) {
+        println("(!) DRY RUN mode")
+    }
+    println("List of pages whose version was updated:")
+    if (listPages.size() > 0) {
+        listPages.each({ pagePath ->
+            println('> Page: ' + pagePath)
+            def isVersionExist = false
+
+            def page = getPage(pagePath);
+            if (page.isLocked()) {
+                if (!dryRun) {
+                    page.unlock()
+                }
+                println('(!) INFO: Page was unlocked')
+            }
+
+            pageManager.getRevisions(pagePath, null, false).each({ revision ->
+                if (revision.getLabel().contains(versionAndPackageName)) {
+                    isVersionExist = true
+                    println('(!) INFO: Page Version already exists')
+                    return false
+                }
+            })
+
+            if (!isVersionExist) {
+                def date = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date());
+                def label = String.format("%s - %s", versionAndPackageName, date);
+
+                if (!dryRun) {
+                    pageManager.createRevision(page, label, "Groovy Script version");
+                }
+                println('(!) INFO: New Page Version was created')
+            }
+        })
+    }
+}
+
+//create or update the package funtion
+def createOrUpdatePackage() {
+    def definitionNode
+
+    if (session.nodeExists(packageDefinitionPath)) {
+        definitionNode = getNode(packageDefinitionPath)
+        println "(!) INFO: A package with this name already exists"
+    } else {
+        def fileNode = getNode(packagesPath).addNode("${versionAndPackageName}.zip", "nt:file")
+
+        def contentNode = fileNode.addNode("jcr:content", "nt:resource")
+
+        contentNode.addMixin("vlt:Package")
+        contentNode.set("jcr:mimeType", "application/zip")
+
+        def stream = new ByteArrayInputStream("".bytes)
+        def binary = session.valueFactory.createBinary(stream)
+
+        contentNode.set("jcr:data", binary)
+
+        definitionNode = contentNode.addNode("vlt:definition", "vlt:PackageDefinition")
+        definitionNode.set("sling:resourceType", "cq/packaging/components/pack/definition")
+        definitionNode.set("name", versionAndPackageName)
+        definitionNode.set("path", "$packagesPath/$versionAndPackageName")
+    }
+
+    definitionNode
+}
+
+//package filter nodes
+def packageFilterNodes(definitionNode) {
+    def filterNode
+
+    if (definitionNode.hasNode("filter")) {
+        filterNode = definitionNode.getNode("filter")
+        filterNode.nodes.each {
+            it.remove()
+        }
+    } else {
+        filterNode = definitionNode.addNode("filter")
+        filterNode.set("sling:resourceType", "cq/packaging/components/pack/definition/filterlist")
+    }
+
+    filterNode
+}
+
+def createBackupPackage(listPages) {
+    if (dryRun) {
+        println "(!) DRY RUN mode"
+    }
+
+    def definitionNode = createOrUpdatePackage()
+    def filterNode = packageFilterNodes(definitionNode)
+
+    listPages.eachWithIndex {
+        path,
+        i ->
+            def f = filterNode.addNode("filter$i")
+
+            f.set("mode", "replace")
+            f.set("root", path + "/jcr:content")
+            f.set("rules", new String[0])
+    }
+
+    if (!dryRun) {
+        save()
+    }
+
+    println "> Please go to '/crx/packmgr/index.jsp' and build created package: " + versionAndPackageName
+}
