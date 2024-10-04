5317f87c280b1beb732d4b0067f0e5906645022a
Haikal Hafiz Kadar <haikalhafiz.kadar@dhl.com>
2024-10-04 15:27:31 +0800

    update branch

--- core/src/main/java/com/positive/dhl/core/models/DhlPage.java
+++ core/src/main/java/com/positive/dhl/core/models/DhlPage.java
@@ -107,6 +107,7 @@ public class DhlPage {
 		String customOgTagImage = properties.get("ogtagimage", pageImage);
 		ogtagimage = StringUtils.isNotBlank(customOgTagImage)
 				? (HTTPS_PREFIX + akamaiHostname + assetprefix).concat(customOgTagImage.trim())
+
 				: HTTPS_PREFIX + akamaiHostname + "/etc.clientlibs/dhl/clientlibs/discover/resources/img/icons/200.png";
 	}
 
