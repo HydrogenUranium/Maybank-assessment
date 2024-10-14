f68f39a89d024c0a5ceed2e639174c3132872144
Dmytro Bratchun <Dmytro_Bratchun@epam.com>
2024-10-14 12:38:50 +0300

    DIS-884 add ArticleListTest coverage

--- core/src/main/java/com/positive/dhl/core/models/ArticleList.java
+++ core/src/main/java/com/positive/dhl/core/models/ArticleList.java
@@ -7,47 +7,31 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.UtilityClass;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-/**
- *
- */
 @Model(adaptables=Resource.class)
-@UtilityClass
 public class ArticleList {
-	@ValueMapValue
+	@ChildResource
 	@Named("items")
 	@Optional
-	private static Resource linksResource;
+	private Resource linksResource;
 
-	private static List<Article> articles;
-	
-    /**
-	 * 
-	 */
-	public static List<Article> getArticles() {
-		return new ArrayList<>(articles);
-	}
+	@Setter
+	@Getter
+	private List<Article> articles;
 
-    /**
-	 * 
-	 */
-	public static void setArticles(List<Article> articles) {
-		ArticleList.articles = new ArrayList<>(articles);
-	}
-
-    /**
-	 * 
-	 */
 	@PostConstruct
-	protected static void  init() {
+	protected void  init() {
 		articles = new ArrayList<>();
 		if (linksResource != null) {
-			int count = 0;
+			var count = 0;
 			Iterator<Resource> linkResources = linksResource.listChildren();
 			while (linkResources.hasNext()) {
 				Article link = linkResources.next().adaptTo(Article.class);
