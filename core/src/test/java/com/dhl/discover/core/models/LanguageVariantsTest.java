c6ba5578b97b964c38d88e479828dbd6b1be6069
Haikal Hafiz Kadar <haikalhafiz.kadar@dhl.com>
2024-11-15 10:59:04 +0800

    fix sonarqube

--- core/src/test/java/com/dhl/discover/core/models/LanguageVariantsTest.java
+++ core/src/test/java/com/dhl/discover/core/models/LanguageVariantsTest.java
@@ -1,20 +1,16 @@
 package com.dhl.discover.core.models;
-
 import com.dhl.discover.core.services.PageUtilService;
 import com.google.common.collect.Maps;
 import com.dhl.discover.core.services.LaunchService;
-import com.dhl.discover.core.services.PageUtilService;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
 import static com.dhl.discover.junitUtils.Constants.NEW_CONTENT_STRUCTURE_JSON;
@@ -29,7 +25,7 @@ class LanguageVariantsTest {
     private final AemContext context = new AemContext();
 
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
         context.load().json(NEW_CONTENT_STRUCTURE_JSON, ROOT_TEST_PAGE_PATH);
 
         var launchService = context.registerService(LaunchService.class, new LaunchService());
--- core/src/test/java/com/dhl/discover/core/services/PageUtilServiceTest.java
+++ core/src/test/java/com/dhl/discover/core/services/PageUtilServiceTest.java
@@ -11,8 +11,7 @@ import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.reflect.Field;
