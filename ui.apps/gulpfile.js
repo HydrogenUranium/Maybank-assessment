79c281e6aeb3ad5dacc984943c917d5c3d61b0ef
Farid Izzat Zanuar <farid.izzat@dhl.com>
2025-01-03 09:25:59 +0800

    modify gulpfile.js

--- ui.apps/gulpfile.js
+++ ui.apps/gulpfile.js
@@ -10,7 +10,7 @@ const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
-const ordered = require('ordered-read-streams'); 
+const ordered = require('ordered-read-streams');
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const eslint = require('gulp-eslint-new');
@@ -67,78 +67,78 @@ gulp.task('sass', finished => {
   finished();
 });
 
-// javascript lint task
-gulp.task('lint', () => {
-  return gulp.src(['./js/dev/**/*.js', '!./js/dev/vendor/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
-// javascript task
-// disable linting, because all the 'old' javascript re-included here doesn't pass
-// gulp.task('javascript', ['lint'], () => {
-gulp.task('javascript', () => {
-  let scriptsPath = './js/dev';
-  let scriptDest = './src/main/content/jcr_root/apps/dhl/clientlibs/discover/js';
-  let scriptDestAnimated = './src/main/content/jcr_root/apps/dhl/clientlibs/discover-animatedpages/js';
-  // Create bundle
-  if (bundler === null) {
-    bundler = browserify(path.join(scriptsPath, 'main.js'), { debug: isDev }).transform(babel, {presets: ['@babel/preset-env']});
-    animatedBundler = browserify(path.join(scriptsPath, 'animated.js'), { debug: isDev }).transform(babel, {presets: ['@babel/preset-env']});
-  }
-
-  let vendor = gulp.src(path.join(scriptsPath, 'vendor', '/**/*.js'))
-    .pipe(gulpIf(isDev, sourcemaps.init()))
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(scriptDest))
-    // .pipe(gulpIf(!isDev, uglify())) to be removed if 'gulp-terser' works
-    .pipe(gulpIf(!isDev, terser()))
-    .pipe(rename('vendor.min.js'))
-    .pipe(gulpIf(isDev, sourcemaps.write('./sourcemaps', {
-      sourceRoot: '/',
-      includeContent: false
-    })))
-    .pipe(gulp.dest(scriptDest));
-
-  let app = bundler.bundle()
-    .on('error', (err) => {
-      console.error(err);
-      this.emit('end');
-    })
-    .pipe(source('main.js'))
-    .pipe(gulp.dest(scriptDest))
-    .pipe(buffer())
-    .pipe(gulpIf(isDev, sourcemaps.init({ loadMaps: true })))
-    // .pipe(gulpIf(!isDev, uglify())) to be removed if 'gulp-terser' works
-    .pipe(gulpIf(!isDev, terser()))
-    .pipe(rename('main.min.js'))
-    .pipe(gulpIf(isDev, sourcemaps.write('./sourcemaps', {
-      sourceRoot: '/',
-      includeContent: false
-    })))
-    .pipe(gulp.dest(scriptDest));
-
-  let animatedApp = animatedBundler.bundle()
-    .on('error', (err) => {
-      console.error(err);
-      this.emit('end');
-    })
-    .pipe(source('animated.js'))
-    .pipe(gulp.dest(scriptDestAnimated))
-    .pipe(buffer())
-    .pipe(gulpIf(isDev, sourcemaps.init({ loadMaps: true })))
-    // .pipe(gulpIf(!isDev, uglify())) to be removed if 'gulp-terser' works
-    .pipe(gulpIf(!isDev, terser()))
-    .pipe(rename('animated.min.js'))
-    .pipe(gulpIf(isDev, sourcemaps.write('./sourcemaps', {
-      sourceRoot: '/',
-      includeContent: false
-    })))
-    .pipe(gulp.dest(scriptDestAnimated));
-
-  return ordered([vendor, app, animatedApp]);
-});
+// // javascript lint task
+// gulp.task('lint', () => {
+//   return gulp.src(['./js/dev/**/*.js', '!./js/dev/vendor/**'])
+//     .pipe(eslint())
+//     .pipe(eslint.format())
+//     .pipe(eslint.failAfterError());
+// });
+//
+// // javascript task
+// // disable linting, because all the 'old' javascript re-included here doesn't pass
+// // gulp.task('javascript', ['lint'], () => {
+// gulp.task('javascript', () => {
+//   let scriptsPath = './js/dev';
+//   let scriptDest = './src/main/content/jcr_root/apps/dhl/clientlibs/discover/js';
+//   let scriptDestAnimated = './src/main/content/jcr_root/apps/dhl/clientlibs/discover-animatedpages/js';
+//   // Create bundle
+//   if (bundler === null) {
+//     bundler = browserify(path.join(scriptsPath, 'main.js'), { debug: isDev }).transform(babel, {presets: ['@babel/preset-env']});
+//     animatedBundler = browserify(path.join(scriptsPath, 'animated.js'), { debug: isDev }).transform(babel, {presets: ['@babel/preset-env']});
+//   }
+//
+//   let vendor = gulp.src(path.join(scriptsPath, 'vendor', '/**/*.js'))
+//     .pipe(gulpIf(isDev, sourcemaps.init()))
+//     .pipe(concat('vendor.js'))
+//     .pipe(gulp.dest(scriptDest))
+//     // .pipe(gulpIf(!isDev, uglify())) to be removed if 'gulp-terser' works
+//     .pipe(gulpIf(!isDev, terser()))
+//     .pipe(rename('vendor.min.js'))
+//     .pipe(gulpIf(isDev, sourcemaps.write('./sourcemaps', {
+//       sourceRoot: '/',
+//       includeContent: false
+//     })))
+//     .pipe(gulp.dest(scriptDest));
+//
+//   let app = bundler.bundle()
+//     .on('error', (err) => {
+//       console.error(err);
+//       this.emit('end');
+//     })
+//     .pipe(source('main.js'))
+//     .pipe(gulp.dest(scriptDest))
+//     .pipe(buffer())
+//     .pipe(gulpIf(isDev, sourcemaps.init({ loadMaps: true })))
+//     // .pipe(gulpIf(!isDev, uglify())) to be removed if 'gulp-terser' works
+//     .pipe(gulpIf(!isDev, terser()))
+//     .pipe(rename('main.min.js'))
+//     .pipe(gulpIf(isDev, sourcemaps.write('./sourcemaps', {
+//       sourceRoot: '/',
+//       includeContent: false
+//     })))
+//     .pipe(gulp.dest(scriptDest));
+//
+//   let animatedApp = animatedBundler.bundle()
+//     .on('error', (err) => {
+//       console.error(err);
+//       this.emit('end');
+//     })
+//     .pipe(source('animated.js'))
+//     .pipe(gulp.dest(scriptDestAnimated))
+//     .pipe(buffer())
+//     .pipe(gulpIf(isDev, sourcemaps.init({ loadMaps: true })))
+//     // .pipe(gulpIf(!isDev, uglify())) to be removed if 'gulp-terser' works
+//     .pipe(gulpIf(!isDev, terser()))
+//     .pipe(rename('animated.min.js'))
+//     .pipe(gulpIf(isDev, sourcemaps.write('./sourcemaps', {
+//       sourceRoot: '/',
+//       includeContent: false
+//     })))
+//     .pipe(gulp.dest(scriptDestAnimated));
+//
+//   return ordered([vendor, app, animatedApp]);
+// });
 
 gulp.task('copyFlagIcons', () => {
   return gulp.src('node_modules/flag-icons/flags/**/*')
@@ -146,9 +146,8 @@ gulp.task('copyFlagIcons', () => {
 });
 
 // build task
-gulp.task('build', gulp.series('sass', 'javascript', 'copyFlagIcons'), finished => {
+gulp.task('build', gulp.series('sass', 'copyFlagIcons'), finished => {
   gulp.src(['./sass/*.css', '!./node_modules/**/*']).pipe(gulp.dest('./build/sass'));
-  gulp.src(['./js/mini/*.min.js', '!./node_modules/**/*']).pipe(gulp.dest('./build/js/mini'));
   gulp.src(['./**/*.php', '!./node_modules/**/*', '!./build/**/*']).pipe(gulp.dest('./build'));
   gulp.src(['./fonts/**/*', '!./node_modules/**/*', '!./build/**/*']).pipe(gulp.dest('./build/fonts'));
   gulp.src(['./**/*.gif', './**/*.jpg', './**/*.jpeg', './**/*.png', './**/*.ico', './**/*.svg',
@@ -157,4 +156,4 @@ gulp.task('build', gulp.series('sass', 'javascript', 'copyFlagIcons'), finished 
 });
 
 // default task
-gulp.task('default', gulp.series('sass', 'javascript', 'copyFlagIcons'));
+gulp.task('default', gulp.series('sass', 'copyFlagIcons'));
