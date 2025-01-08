4fc4f150ee7a35525b1c054aefdade5e42275218
Farid Izzat Zanuar <farid.izzat@dhl.com>
2025-01-08 10:21:58 +0800

    sanitize description for blogpost

--- core/src/main/java/com/dhl/discover/core/services/schema/impl/BlogPostingSchemaAdapter.java
+++ core/src/main/java/com/dhl/discover/core/services/schema/impl/BlogPostingSchemaAdapter.java
@@ -7,6 +7,7 @@ import com.dhl.discover.core.services.PageUtilService;
 import com.dhl.discover.core.services.PathUtilService;
 import com.dhl.discover.core.services.schema.AbstractSchemaAdapter;
 import com.dhl.discover.core.services.schema.SchemaAdapter;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.osgi.service.component.annotations.Component;
@@ -57,7 +58,7 @@ public class BlogPostingSchemaAdapter extends AbstractSchemaAdapter {
         blogPosting.add("mainEntityOfPage", webPage);
 
         blogPosting.addProperty("headline", article.getTitle());
-        blogPosting.addProperty("description", article.getDescription());
+        blogPosting.addProperty("description", StringEscapeUtils.escapeHtml4((article.getDescription())));
         blogPosting.addProperty("image", pathUtilService.getFullMappedPath(article.getHeroimagemob(), request));
 
         JsonObject author = createType(ORGANIZATION);
