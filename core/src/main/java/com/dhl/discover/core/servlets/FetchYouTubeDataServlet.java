855467675eeb4544f7a229caba9c5e2416a2e83e
Haikal Hafiz Kadar <haikalhafiz.kadar@dhl.com>
2025-02-12 11:38:20 +0800

    rollback servlet path register

--- core/src/main/java/com/dhl/discover/core/servlets/FetchYouTubeDataServlet.java
+++ core/src/main/java/com/dhl/discover/core/servlets/FetchYouTubeDataServlet.java
@@ -33,9 +33,7 @@ import java.nio.charset.StandardCharsets;
 @Component(service = Servlet.class, property = {
         Constants.SERVICE_DESCRIPTION + "=YouTube Schema Markup",
         "sling.servlet.methods=" + HttpConstants.METHOD_GET,
-        "sling.servlet.resourceTypes=" + "discoverdhlapi/youtube",
-        "sling.servlet.selectors=" + "index",
-        "sling.servlet.extensions=" + "json"}
+        "sling.servlet.paths=" + "/apps/dhl/discoverdhlapi/youtube/index.json"}
 )
 @Designate(ocd = FetchYouTubeDataServlet.Configuration.class)
 @Slf4j
