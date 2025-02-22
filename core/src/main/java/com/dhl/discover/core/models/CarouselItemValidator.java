70bc2973a5e6e44413e2b0fbb0ec239679f62321
Haikal Hafiz Kadar <haikalhafiz.kadar@dhl.com>
2025-02-22 23:29:35 +0800

    Add HTL condition and model for CarouselItemValidator

---
+++ core/src/main/java/com/dhl/discover/core/models/CarouselItemValidator.java
@@ -0,0 +1,48 @@
+package com.dhl.discover.core.models;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Slf4j
+@Getter
+public class CarouselItemValidator {
+
+    static final String PAGE_PATH="nodePath";
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private final SlingHttpServletRequest slingRequest;
+
+    private String nodePath;
+
+    private boolean nodeExists;
+
+    @Inject
+    public CarouselItemValidator(SlingHttpServletRequest slingRequest) {
+        this.slingRequest = slingRequest;
+    }
+    @PostConstruct
+    protected void init() {
+        nodePath = (String) slingRequest.getAttribute(PAGE_PATH);
+
+        if (nodePath != null && !nodePath.isEmpty()) {
+            Resource resource = resourceResolver.getResource(nodePath);
+            log.info("Resource haikal : {}", resource);
+            nodeExists = (resource != null);
+        } else {
+            nodeExists = false;
+        }
+    }
+
+}
