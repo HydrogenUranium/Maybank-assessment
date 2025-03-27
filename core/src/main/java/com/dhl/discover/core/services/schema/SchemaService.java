7c8bd590d3ae4bfd0b15ffbaaa818c94ed525c8a
Anchal Todariya <anchal.todariya@dhl.com>
2025-03-27 10:17:57 +0530

    DIS-1154 - Breadcrumb schema markup implementation

--- core/src/main/java/com/dhl/discover/core/services/schema/SchemaService.java
+++ core/src/main/java/com/dhl/discover/core/services/schema/SchemaService.java
@@ -1,11 +1,10 @@
 package com.dhl.discover.core.services.schema;
 
-import com.adobe.cq.wcm.core.components.models.Breadcrumb;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.factory.ModelFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
@@ -13,15 +12,20 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.adobe.cq.wcm.core.components.models.Breadcrumb;
+import java.util.function.Predicate;
+
 
 @Component(service = SchemaService.class)
 @Slf4j
 public class SchemaService {
 
-    private final List<SchemaAdapter> adapters = new ArrayList<>();
-    public static final String BREADCRUMB_RESOURCE_TYPE = "dhl/components/content/breadcrumb";
+    @Reference
+    private ModelFactory modelFactory;
 
+    private final List<SchemaAdapter> adapters = new ArrayList<>();
+    public static final String CQ_TEMPLATE = "cq:template";
+    public static final String TYPE_EDITABLE = "editable";
+    public static final String BREADCRUMB_TEMPLATE_PATH = "/structure/jcr:content";
     @Reference(
             service = SchemaAdapter.class,
             cardinality = ReferenceCardinality.MULTIPLE,
@@ -43,21 +47,18 @@ public class SchemaService {
 
     public List<String> getSchemas(Resource resource, SlingHttpServletRequest request) {
         List<String> schemas = new ArrayList<>();
-        collectSchemas(resource, request, schemas);
+        collectSchemas(resource, request, schemas, r -> r != null);
+        if( resource != null) {
+            Resource templateStructureRoot = resource.getResourceResolver().getResource(resource.getValueMap().get(CQ_TEMPLATE, "") + BREADCRUMB_TEMPLATE_PATH);
+            collectSchemas(templateStructureRoot, request, schemas, r -> r != null && !r.getValueMap().get(TYPE_EDITABLE, false));
+        }
         return schemas;
     }
 
-    private void collectSchemas(Resource resource, SlingHttpServletRequest request, List<String> list) {
-        if(resource == null) {
+    private void collectSchemas(Resource resource, SlingHttpServletRequest request, List<String> list, Predicate<Resource> predicate) {
+        if(resource == null || !predicate.test(resource)) {
             return;
         }
-        if(resource.isResourceType(BREADCRUMB_RESOURCE_TYPE)) {
-            JsonObject breadcrumbJson = handleBreadcrumbRequest(request);
-            if(breadcrumbJson != null) {
-                list.add(breadcrumbJson.toString());
-            }
-        }
-
         var adapter = getSchemaAdapter(resource);
         if (adapter != null) {
             JsonObject json = adapter.toJson(resource, request);
@@ -65,33 +66,6 @@ public class SchemaService {
                 list.add(json.toString());
             }
         }
-        resource.getChildren().forEach(child -> collectSchemas(child, request, list));
-    }
-
-    public JsonObject handleBreadcrumbRequest(SlingHttpServletRequest request) {
-        Breadcrumb breadcrumb = request.adaptTo(Breadcrumb.class);
-        if (breadcrumb == null) {
-            log.warn("Breadcrumb component could not be adapted from the request.");
-            return null;
-        }
-
-        JsonObject breadcrumbJson = new JsonObject();
-        breadcrumbJson.addProperty("type", "BreadcrumbList");
-        breadcrumbJson.addProperty("context", "http://schema.org");
-
-        JsonArray itemListElement = new JsonArray();
-        /*List<Breadcrumb.Item> items = breadcrumb.getItems();
-        for (int i = 0; i < items.size(); i++) {
-            Breadcrumb.Item item = items.get(i);
-            JsonObject listItem = new JsonObject();
-            listItem.addProperty("type", "ListItem");
-            listItem.addProperty("position", i + 1);
-            listItem.addProperty("name", item.getTitle());
-            listItem.addProperty("item", item.getPath());
-            itemListElement.add(listItem);
-        }*/
-
-        breadcrumbJson.add("itemListElement", itemListElement);
-        return breadcrumbJson;
+        resource.getChildren().forEach(child -> collectSchemas(child, request, list, predicate));
     }
 }
--- core/src/main/java/com/dhl/discover/core/services/schema/impl/BreadcrumbSchemaAdapter.java
+++ core/src/main/java/com/dhl/discover/core/services/schema/impl/BreadcrumbSchemaAdapter.java
@@ -1,32 +1,31 @@
 package com.dhl.discover.core.services.schema.impl;
 
-import com.dhl.discover.core.services.PageContentExtractorService;
+import com.adobe.cq.wcm.core.components.models.Breadcrumb;
+import com.adobe.cq.wcm.core.components.models.NavigationItem;
 import com.dhl.discover.core.services.schema.SchemaAdapter;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.factory.ModelFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import java.util.List;
 
 import static com.dhl.discover.core.constants.SchemaMarkupConstants.*;
 import static com.dhl.discover.core.constants.SchemaMarkupType.*;
-import static com.dhl.discover.core.utils.SchemaMarkupUtils.createSchema;
-import static com.dhl.discover.core.utils.SchemaMarkupUtils.createType;
+import static com.dhl.discover.core.utils.SchemaMarkupUtils.*;
 
-/*@Component(service = SchemaAdapter.class)
+@Component(service = SchemaAdapter.class)
 public class BreadcrumbSchemaAdapter implements SchemaAdapter {
     public static final String RESOURCE_TYPE = "dhl/components/content/breadcrumb";
-    private static final String PROPERTY_IS_BREADCRUMNB = "breadcrumb";
-    private static final String PROPERTY_PANEL_TITLE = "cq:panelTitle";
 
     @Reference
-    private PageContentExtractorService extractorService;
+    private ModelFactory modelFactory;
 
     @Override
     public boolean canHandle(Resource resource) {
-        var valueMap = resource.getValueMap();
         return resource.isResourceType(RESOURCE_TYPE);
     }
 
@@ -36,30 +35,29 @@ public class BreadcrumbSchemaAdapter implements SchemaAdapter {
         return StringUtils.normalizeSpace(noHtml.replaceAll("[\\r\\n]+", " "));
     }
 
-    private JsonObject getEntry(Resource resource, int position) {
-        var valueMap = resource.getValueMap();
-        var nameString = valueMap.get(NAME, "");
-
+    private JsonObject getEntry(NavigationItem item, int position) {
         JsonObject mainEntity = createType(LIST_ITEM);
-        mainEntity.addProperty(ITEM, nameString);
-        mainEntity.addProperty("position", position);
-
-        // Add additional properties if needed
-        mainEntity.addProperty("url", valueMap.get("url", ""));
-        mainEntity.addProperty("name", removeHtmlTags(nameString));
+        mainEntity.addProperty(POSITION, position);
+        mainEntity.addProperty(NAME, removeHtmlTags(item.getTitle()));
+        mainEntity.addProperty(ITEM, item.getPath());
         return mainEntity;
     }
 
     @Override
     public JsonObject toJson(Resource resource, SlingHttpServletRequest request) {
         JsonObject json = createSchema(BREADCRUMB_LIST);
+        JsonObject breadcrumbJson = new JsonObject();
+        breadcrumbJson.addProperty(TYPE_FIELD, BREADCRUMB_LIST.toString());
+        breadcrumbJson.addProperty(CONTEXT_FIELD, HTTPS_SCHEMA_ORG_CONTEXT);
+        List<NavigationItem> items = (List<NavigationItem>) modelFactory.createModelFromWrappedRequest(request, resource, Breadcrumb.class).getItems();
 
         var jsonArray = new JsonArray();
         int position = 1;
-        for (Resource child : resource.getChildren()) {
-            jsonArray.add(getEntry(child, position++));
+        for (NavigationItem item : items) {
+            jsonArray.add(getEntry(item, position++));
         }
         json.add(ITEM_LIST_ELEMENT, jsonArray);
         return json;
     }
-}*/
+
+}
