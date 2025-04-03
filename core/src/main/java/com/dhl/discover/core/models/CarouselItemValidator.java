4ed4cce5f3d8e3bd0d63d4d61960b932869e60a7
Haikal Hafiz Kadar <haikalhafiz.kadar@dhl.com>
2025-04-03 17:46:41 +0800

    remove unused import

--- core/src/main/java/com/dhl/discover/core/models/CarouselItemValidator.java
+++ core/src/main/java/com/dhl/discover/core/models/CarouselItemValidator.java
@@ -1,7 +1,6 @@
 package com.dhl.discover.core.models;
 
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
