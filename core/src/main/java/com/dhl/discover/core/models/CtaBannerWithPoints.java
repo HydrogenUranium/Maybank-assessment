436002298f7d4a9064362ba3d9269cfc7af09ba9
Haikal Hafiz Kadar <haikalhafiz.kadar@dhl.com>
2025-05-08 09:50:18 +0800

    roll back CtaBannerWithPoints

--- core/src/main/java/com/dhl/discover/core/models/CtaBannerWithPoints.java
+++ core/src/main/java/com/dhl/discover/core/models/CtaBannerWithPoints.java
@@ -1,14 +1,16 @@
 package com.dhl.discover.core.models;
 
+import com.dhl.discover.core.injectors.InjectHomeProperty;
 import lombok.Getter;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -23,41 +25,154 @@ public class CtaBannerWithPoints {
     @Optional
     private Resource pointsMultifield;
 
+    @InjectHomeProperty
+    @Named("multifields/ctaBanner-subscribeToOurNewsletter-points")
+    @Optional
+    private Resource pointsMultifieldSubscribe;
+
+    @InjectHomeProperty
+    @Named("multifields/ctaBanner-openBusinessAccount-points")
+    @Optional
+    private Resource pointsMultifieldOpenBusinessAccount;
+
+    @ValueMapValue
+    @Optional
+    @Default(values = "custom")
+    private String type;
+
     @ValueMapValue
     @Optional
     @Getter
     private String title;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-subscribeToOurNewsletter-title")
+    @Optional
+    private String titleSubscribe;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-openBusinessAccount-title")
+    @Optional
+    private String titleOpenBusinessAccount;
 
     @ValueMapValue
     @Getter
     @Optional
     private String mobileBackgroundImage;
 
+    @InjectHomeProperty
+    @Named("ctaBanner-subscribeToOurNewsletter-mobileBackgroundImage")
+    @Optional
+    private String mobileBackgroundImageSubscribe;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-openBusinessAccount-mobileBackgroundImage")
+    @Optional
+    private String mobileBackgroundImageOpenBusinessAccount;
+
     @ValueMapValue
     @Getter
     @Optional
     private String tabletBackgroundImage;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-subscribeToOurNewsletter-tabletBackgroundImage")
+    @Optional
+    private String tabletBackgroundImageSubscribe;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-openBusinessAccount-tabletBackgroundImage")
+    @Optional
+    private String tabletBackgroundImageOpenBusinessAccount;
 
     @ValueMapValue
     @Getter
     @Optional
     private String desktopBackgroundImage;
 
+    @InjectHomeProperty
+    @Named("ctaBanner-subscribeToOurNewsletter-desktopBackgroundImage")
+    @Optional
+    private String desktopBackgroundImageSubscribe;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-openBusinessAccount-desktopBackgroundImage")
+    @Optional
+    private String desktopBackgroundImageOpenBusinessAccount;
+
     @ValueMapValue
     @Getter
     @Optional
     private String buttonName;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-subscribeToOurNewsletter-buttonName")
+    @Optional
+    private String buttonNameSubscribe;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-openBusinessAccount-buttonName")
+    @Optional
+    private String buttonNameOpenBusinessAccount;
 
     @ValueMapValue
     @Getter
     @Optional
     private String buttonLink;
 
+    @InjectHomeProperty
+    @Named("ctaBanner-subscribeToOurNewsletter-buttonLink")
+    @Optional
+    private String buttonLinkSubscribe;
+
+    @InjectHomeProperty
+    @Named("ctaBanner-openBusinessAccount-buttonLink")
+    @Optional
+    private String buttonLinkOpenBusinessAccount;
+
     @Getter
     private final String id = "cta-banner_" + UUID.randomUUID();
 
     @Getter
     private List<String> points = new ArrayList<>();
+
+    @PostConstruct
+    protected void init() {
+        switch (type) {
+            case ("businessAccount"):
+                initOpenBusinessAccount();
+                break;
+            case ("subscribeNewsletter"):
+                initSubscribe();
+                break;
+            default:
+                initCustom();
+        }
+    }
+
+    private void initCustom() {
+        points = extractPoints(pointsMultifield);
+    }
+
+    private void initSubscribe() {
+        title = titleSubscribe;
+        points = extractPoints(pointsMultifieldSubscribe);
+        mobileBackgroundImage = mobileBackgroundImageSubscribe;
+        tabletBackgroundImage = tabletBackgroundImageSubscribe;
+        desktopBackgroundImage = desktopBackgroundImageSubscribe;
+        buttonName = buttonNameSubscribe;
+        buttonLink = buttonLinkSubscribe;
+    }
+
+    private void initOpenBusinessAccount() {
+        title = titleOpenBusinessAccount;
+        points = extractPoints(pointsMultifieldOpenBusinessAccount);
+        mobileBackgroundImage = mobileBackgroundImageOpenBusinessAccount;
+        tabletBackgroundImage = tabletBackgroundImageOpenBusinessAccount;
+        desktopBackgroundImage = desktopBackgroundImageOpenBusinessAccount;
+        buttonName = buttonNameOpenBusinessAccount;
+        buttonLink = buttonLinkOpenBusinessAccount;
+    }
 
     private List<String> extractPoints(Resource pointsMultifield) {
         List<String> list = new ArrayList<>();
--- core/src/test/java/com/dhl/discover/core/models/CtaBannerWithPointsTest.java
+++ core/src/test/java/com/dhl/discover/core/models/CtaBannerWithPointsTest.java
@@ -1,13 +1,16 @@
 package com.dhl.discover.core.models;
 
+import com.day.cq.wcm.api.Page;
 import com.dhl.discover.core.injectors.HomePropertyInjector;
 import com.dhl.discover.core.services.PageUtilService;
 import com.dhl.discover.core.services.PathUtilService;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.spi.Injector;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,13 +19,22 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @ExtendWith({AemContextExtension.class, MockitoExtension.class})
 class CtaBannerWithPointsTest {
     private final static String COMPONENT_LOCATION = "/content/home/small-business-advice/article/jcr:content/root/article_container/body/responsivegrid";
 
     private final AemContext context = new AemContext(ResourceResolverType.JCR_MOCK);
+
+    private final MockSlingHttpServletRequest request = context.request();
+
+    private final ResourceResolver resourceResolver = context.resourceResolver();
+
     @Mock
     private PathUtilService pathUtilService;
 
@@ -40,6 +52,52 @@ class CtaBannerWithPointsTest {
         context.addModelsForClasses(CtaBannerWithPoints.class);
     }
 
+    private void mockHomePage() {
+        when(pageUtils.getHomePage(any(Page.class))).thenReturn(context.resourceResolver().getResource("/content/home").adaptTo(Page.class));
+    }
+
+    private void initRequest(String path) {
+        request.setPathInfo(path);
+        request.setResource(resourceResolver.getResource(path));
+    }
+
+    @Test
+    void init_ShouldInitPropertiesFromHomePage_WhenTypeIsSubscribe() {
+        initRequest(COMPONENT_LOCATION + "/cta_banner_subscribeNewsletter");
+        mockHomePage();
+
+        CtaBannerWithPoints ctaBannerWithPoints = request.adaptTo(CtaBannerWithPoints.class);
+
+        assertEquals("SUBSCRIBE TO OUR NEWSLETTER", ctaBannerWithPoints.getTitle());
+        assertEquals("/content/dhl/subscribe", ctaBannerWithPoints.getButtonLink());
+        assertEquals("Subscribe", ctaBannerWithPoints.getButtonName());
+        assertEquals("/content/dam/images/subscribe-desktop.png", ctaBannerWithPoints.getDesktopBackgroundImage());
+        assertEquals("/content/dam/images/subscribe-tablet.png", ctaBannerWithPoints.getTabletBackgroundImage());
+        assertEquals("/content/dam/images/subscribe-mobile.png", ctaBannerWithPoints.getMobileBackgroundImage());
+        assertEquals(Arrays.asList(
+                "Fortnightly insights, tips and free assets",
+                "Shape a global audience for your business"
+        ), ctaBannerWithPoints.getPoints());
+    }
+
+    @Test
+    void init_ShouldInitPropertiesFromHomePage_WhenTypeIsOpenBusinessAccount() {
+        initRequest(COMPONENT_LOCATION + "/cta_banner_businessAccount");
+        mockHomePage();
+
+        CtaBannerWithPoints ctaBannerWithPoints = request.adaptTo(CtaBannerWithPoints.class);
+
+        assertEquals("Open a Business Account", ctaBannerWithPoints.getTitle());
+        assertEquals("/content/dhl/openBusinessAccount", ctaBannerWithPoints.getButtonLink());
+        assertEquals("Join Us", ctaBannerWithPoints.getButtonName());
+        assertEquals("/content/dam/images/desktop.jpg", ctaBannerWithPoints.getDesktopBackgroundImage());
+        assertEquals("/content/dam/images/tablet.jpg", ctaBannerWithPoints.getTabletBackgroundImage());
+        assertEquals("/content/dam/images/mobile.jpg", ctaBannerWithPoints.getMobileBackgroundImage());
+        assertEquals(Arrays.asList(
+                "Fortnightly insights, tips and free assets",
+                "Shape a global audience for your business"
+        ), ctaBannerWithPoints.getPoints());
+    }
 
     @Test
     void init_ShouldInitPropertiesFromHomePage_WhenTypeIsCustom() {
