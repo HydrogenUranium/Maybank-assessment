231a9653838c36ea441d7a5e7d5f5835c6cc7475
Dmytro Bratchun <Dmytro_Bratchun@epam.com>
2025-05-12 15:07:38 +0300

    DIS-1222 fix code duplication

--- core/src/main/java/com/dhl/discover/core/services/impl/DeletionThumbnailWorkflowProcess.java
+++ core/src/main/java/com/dhl/discover/core/services/impl/DeletionThumbnailWorkflowProcess.java
@@ -1,12 +1,19 @@
 package com.dhl.discover.core.services.impl;
 
+import com.day.cq.workflow.WorkflowException;
+import com.day.cq.workflow.WorkflowSession;
 import com.day.cq.workflow.exec.WorkItem;
 import com.day.cq.workflow.exec.WorkflowProcess;
 import com.day.cq.workflow.metadata.MetaDataMap;
 import com.dhl.discover.core.utils.OSGiConfigUtils;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import java.util.Map;
 
+@Slf4j
 public abstract class DeletionThumbnailWorkflowProcess implements WorkflowProcess {
     protected static final String ORIGINAL_IMAGE_NODE_NAME = "original-image";
     protected static final String IMAGE_NODE_NAME = "image";
@@ -28,5 +35,29 @@ public abstract class DeletionThumbnailWorkflowProcess implements WorkflowProces
     protected String getContentNodePath(WorkItem item) {
         return getPayloadPath(item) + "/jcr:content";
     }
+
+    @Override
+    public void execute(WorkItem item, WorkflowSession workflowSession, MetaDataMap args) throws WorkflowException {
+        String contentNodePath = getContentNodePath(item);
+        String deletedImageReference = getDeletedImageReference(args);
+
+        try {
+            var session = workflowSession.getSession();
+            if (session == null || !session.nodeExists(contentNodePath) || deletedImageReference == null) {
+                log.warn("Skipping workflow process for path: {}. Session is null or node does not exist or deletedImageReference is null.", contentNodePath);
+                return;
+            }
+
+            var contentNode = session.getNode(contentNodePath);
+
+            processContentNode(contentNode, deletedImageReference, session);
+
+            session.save();
+        } catch (RepositoryException e) {
+            throw new WorkflowException("Error processing FixPageThumbnailWorkflow at: " + item.getWorkflowData().getPayload(), e);
+        }
+    }
+
+    protected abstract void processContentNode(Node contentNode, String deletedImageReference, Session session) throws RepositoryException;
 
 }
--- core/src/main/java/com/dhl/discover/core/services/impl/RemovePendingDeletionThumbnail.java
+++ core/src/main/java/com/dhl/discover/core/services/impl/RemovePendingDeletionThumbnail.java
@@ -1,62 +1,38 @@
 package com.dhl.discover.core.services.impl;
 
-import com.day.cq.workflow.WorkflowException;
-import com.day.cq.workflow.WorkflowSession;
-import com.day.cq.workflow.exec.WorkItem;
 import com.day.cq.workflow.exec.WorkflowProcess;
-import com.day.cq.workflow.metadata.MetaDataMap;
-import com.dhl.discover.core.utils.OSGiConfigUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.osgi.service.component.annotations.Component;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-import java.util.Map;
 
 @Component(service = WorkflowProcess.class, property = {"process.label=Remove Pending Deletion Thumbnail"})
 @Slf4j
 public class RemovePendingDeletionThumbnail extends DeletionThumbnailWorkflowProcess{
 
     @Override
-    public void execute(WorkItem item, WorkflowSession workflowSession, MetaDataMap args) throws WorkflowException {
-        String contentNodePath = getContentNodePath(item);
-        String deletedImageReference = getDeletedImageReference(args);
+    protected void processContentNode(Node contentNode, String deletedImageReference, Session session) throws RepositoryException {
+        boolean originalExists = contentNode.hasNode(ORIGINAL_IMAGE_NODE_NAME);
 
-        try {
-            Session session = workflowSession.getSession();
-            if (session == null || !session.nodeExists(contentNodePath) || deletedImageReference == null) {
-                log.warn("Skipping workflow process for path: {}. Session is null or node does not exist or deletedImageReference is null.", contentNodePath);
-                return;
+        if (originalExists) {
+            if (contentNode.hasNode(IMAGE_NODE_NAME)) {
+                contentNode.getNode(IMAGE_NODE_NAME).remove();
             }
 
-            Node contentNode = session.getNode(contentNodePath);
+            Node originalImage = contentNode.getNode(ORIGINAL_IMAGE_NODE_NAME);
+            session.move(originalImage.getPath(), contentNode.getPath() + "/" + IMAGE_NODE_NAME);
 
-            boolean originalExists = contentNode.hasNode(ORIGINAL_IMAGE_NODE_NAME);
+        } else if (contentNode.hasNode(IMAGE_NODE_NAME)) {
+            Node imageNode = contentNode.getNode(IMAGE_NODE_NAME);
 
-            if (originalExists) {
-                if (contentNode.hasNode(IMAGE_NODE_NAME)) {
-                    contentNode.getNode(IMAGE_NODE_NAME).remove();
-                }
-
-                Node originalImage = contentNode.getNode(ORIGINAL_IMAGE_NODE_NAME);
-                session.move(originalImage.getPath(), contentNode.getPath() + "/" + IMAGE_NODE_NAME);
-
-            } else if (contentNode.hasNode(IMAGE_NODE_NAME)) {
-                Node imageNode = contentNode.getNode(IMAGE_NODE_NAME);
-
-                if (imageNode.hasProperty(FILE_REFERENCE)) {
-                    String fileRef = imageNode.getProperty(FILE_REFERENCE).getString();
-                    if (deletedImageReference.equals(fileRef)) {
-                        imageNode.remove();
-                    }
+            if (imageNode.hasProperty(FILE_REFERENCE)) {
+                String fileRef = imageNode.getProperty(FILE_REFERENCE).getString();
+                if (deletedImageReference.equals(fileRef)) {
+                    imageNode.remove();
                 }
             }
-
-            session.save();
-
-        } catch (RepositoryException e) {
-            throw new WorkflowException("Error processing FixPageThumbnailWorkflow at: " + item.getWorkflowData().getPayload(), e);
         }
     }
 }
