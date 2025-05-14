2a2348fcfff57530798cda05099c5e1f2f2a1875
Anchal Todariya <anchal.todariya@dhl.com>
2025-05-14 22:38:08 +0530

    DIS-1200 - content cleanup and publish

--- ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-1200-content-cleanup-root-directory.groovy
+++ ui.content/src/main/content/jcr_root/conf/groovyconsole/scripts/aecu/DIS-1200-content-cleanup-root-directory.groovy
@@ -1,10 +1,78 @@
 import groovy.transform.Field
 
-@Field DRY_RUN = true;
+@Field DRY_RUN = false;
 @Field REPLICATE = false;
 @Field LOGGING = true;
-@Field ROOT = "/content/dhl"
 
+@Field ROOTS = [
+
+        '/content/dhl/ae/en-ae',
+        '/content/dhl/at/de-at',
+        '/content/dhl/at/en-at',
+        '/content/dhl/au/en-au',
+        '/content/dhl/bd/en-bd',
+        '/content/dhl/be/en-be',
+        '/content/dhl/be/fr-be',
+        '/content/dhl/be/nl-be',
+        '/content/dhl/br/en-br',
+        '/content/dhl/br/pt-br',
+        '/content/dhl/ca/en-ca',
+        '/content/dhl/ca/fr-ca',
+        '/content/dhl/ch/de-ch',
+        '/content/dhl/ch/en-ch',
+        '/content/dhl/ch/fr-ch',
+        '/content/dhl/ch/it-ch',
+        '/content/dhl/cn/en-cn',
+        '/content/dhl/cn/zh-cn',
+        '/content/dhl/cz/cs-cz',
+        '/content/dhl/cz/en-cz',
+        '/content/dhl/es/en-es',
+        '/content/dhl/es/es-es',
+        '/content/dhl/fr/en-fr',
+        '/content/dhl/fr/fr-fr',
+        '/content/dhl/gb/en-gb',
+        '/content/dhl/global/en-global',
+        '/content/dhl/hk/en-hk',
+        '/content/dhl/hk/zh-hk',
+        '/content/dhl/hu/en-hu',
+        '/content/dhl/hu/hu-hu',
+        '/content/dhl/id/en-id',
+        '/content/dhl/id/id-id',
+        '/content/dhl/ie/en-ie',
+        '/content/dhl/il/en-il',
+        '/content/dhl/il/he-il',
+        '/content/dhl/in/en-in',
+        '/content/dhl/it/en-it',
+        '/content/dhl/it/it-it',
+        '/content/dhl/jp/en-jp',
+        '/content/dhl/jp/ja-jp',
+        '/content/dhl/ke/en-ke',
+        '/content/dhl/kh/en-kh',
+        '/content/dhl/kr/en-kr',
+        '/content/dhl/kr/ko-kr',
+        '/content/dhl/lk/en-lk',
+        '/content/dhl/mm/en-mm',
+        '/content/dhl/my/en-my',
+        '/content/dhl/ng/en-ng',
+        '/content/dhl/nz/en-nz',
+        '/content/dhl/ph/en-ph',
+        '/content/dhl/pk/en-pk',
+        '/content/dhl/pt/en-pt',
+        '/content/dhl/pt/pt-pt',
+        '/content/dhl/se/en-se',
+        '/content/dhl/se/sv-se',
+        '/content/dhl/sg/en-sg',
+        '/content/dhl/sk/en-sk',
+        '/content/dhl/sk/sk-sk',
+        '/content/dhl/th/en-th',
+        '/content/dhl/th/th-th',
+        '/content/dhl/tw/en-tw',
+        '/content/dhl/tw/zh-tw',
+        '/content/dhl/us/en-us',
+        '/content/dhl/vn/en-vn',
+        '/content/dhl/vn/vi-vn',
+        '/content/dhl/za/en-za'
+]
 
 def log(string) {
     if(LOGGING) {
@@ -12,7 +80,7 @@ def log(string) {
     }
 }
 
-/*def getJcrContent(path) {
+def getJcrContent(path) {
     return pageManager.getContainingPage(path).getContentResource();
 }
 
@@ -24,17 +92,18 @@ def isPublished(path) {
 }
 
 def handlePublication(pagePath) {
+    def replicator = getService("com.day.cq.replication.Replicator")
     if(!REPLICATE) {
         return;
     }
     if(isPublished(pagePath)) {
-        replicator.replicate(session, ReplicationActionType.ACTIVATE, it.getPath())
-        log "Publish $it.getPath()"
+        replicator.replicate(session, ReplicationActionType.ACTIVATE, pagePath)
+        log "Publish $pagePath"
     } else {
-        log "Page is not published $it.getPath()"
+        log "Page is not published $pagePath"
     }
 }
-*/
+
 def processSingleNode(page, source) {
     def contentNode = page.getContentResource().adaptTo(Node.class);
 
@@ -92,7 +161,13 @@ def processNode(child) {
         '/conf/dhl/settings/wcm/templates/home-page': (page) -> processNodes(page, ['root/responsivegrid','root/container']),
 ];
 
-def processPage(page) {
+def processPage(ROOTS) {
+    for(path in ROOTS) {
+        doAction(getPage(path))
+    }
+}
+
+def doAction(page) {
     if(page == null) {
         return;
     }
@@ -104,6 +179,7 @@ def processPage(page) {
         if(DRY_RUN) {
             session.refresh(false);
             log "Refresh Changes (---)"
+            handlePublication(page.getPath())
         } else {
             session.save();
             log "Save Changes (+++)"
@@ -113,9 +189,10 @@ def processPage(page) {
     }
 
     def iterator = page.listChildren();
+
     while(iterator.hasNext()) {
-        processPage(iterator.next());
+        doAction(iterator.next());
     }
 }
+processPage(ROOTS);
 
-processPage(pageManager.getPage(ROOT));
