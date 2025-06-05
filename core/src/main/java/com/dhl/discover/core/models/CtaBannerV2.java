1cdc7507c9317d1c142a6bec8d58d074491e3801
Anchal Todariya <anchal.todariya@dhl.com>
2025-06-05 23:02:16 +0530

    DIS-1254- CTA banner V2

--- core/src/main/java/com/dhl/discover/core/models/CtaBannerV2.java
+++ core/src/main/java/com/dhl/discover/core/models/CtaBannerV2.java
@@ -1,9 +1,7 @@
 package com.dhl.discover.core.models;
 
 import com.adobe.cq.wcm.core.components.models.Image;
-import com.day.cq.wcm.api.designer.Style;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -11,7 +9,6 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.*;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
 
 @Model(adaptables = {Resource.class, SlingHttpServletRequest.class},
         defaultInjectionStrategy= DefaultInjectionStrategy.OPTIONAL)
@@ -19,45 +16,16 @@ import java.util.List;
 @Getter
 public class CtaBannerV2 extends AdaptiveImage {
 
-    @ScriptVariable
-    protected Style currentStyle;
-
     @ValueMapValue
-    private String summaryTitle;
+    private String topTitle;
 
-    @ChildResource
-    private List<Point> summaryPoints;
+    private static final boolean disabled = false;
 
     @Self
     private Image defaultImageModel;
 
-    @ValueMapValue
-    private boolean useVideo;
-
-    @ValueMapValue
-    private String video;
-
-    private String videoMimeType;
-
-    private boolean keyTakeAways;
-
     @PostConstruct
     protected void init() {
         super.initModel();
-        
-        if (currentStyle != null) {
-            keyTakeAways = currentStyle.get("keyTakeAways", false);
-        }
-        
-        if (useVideo && StringUtils.isNoneBlank(video) && assetUtilService != null) {
-            videoMimeType = assetUtilService.getMimeType(video);
-        }
-    }
-
-    @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-    @Getter
-    public static class Point {
-        @ValueMapValue
-        private String text;
     }
 }
