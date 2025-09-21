6867f62792a8f04f22e81529073fe60d6fead72d
Dhivya Selvaraj (DHL IT Services) <dhivya.selvaraj@dhl.com>
2025-09-22 01:18:21 +0530

    https://jira.dhl.com/browse/DIS-921

--- ui.apps/js/dev/Components/StickySidebar.js
+++ ui.apps/js/dev/Components/StickySidebar.js
@@ -1,52 +1,66 @@
-
 class StickySidebar {
 
-    init() {
-        this.stickySidebarEvents();
+  init() {
+    this.stickySidebarEvents();
+  }
+/*
+stickySidebarEvents() {
+
+  const sidebar = document.querySelector('.cmp-article-container__sidebar-wrapper');
+  const sidebarContent = sidebar.querySelector('.sidebar-cmp-with-sticky-element');
+  if (!sidebar) return;
+
+  const containerGrid = document.querySelector('.container.responsivegrid.aem-GridColumn.aem-GridColumn--default--12');
+  const originalWidth = sidebarContent.offsetWidth;
+  const containerWidth = containerGrid ? containerGrid.offsetWidth : originalWidth;
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    const viewportHeight = window.innerHeight;
+
+
+    const sidebarRect = document.getElementsByClassName('cmp-article-container__sidebar-wrapper')[0];
+    //const sidebarContent = document.getElementsByClassName('cmp-experiencefragment--sidebar')[0];
+    const contentHeight = sidebarContent.scrollHeight;
+    const sidebarTop = sidebarRect.getBoundingClientRect().top + scrollY;
+
+    console.log("sidebarTop::", sidebarTop);
+    console.log("viewportHeight::", viewportHeight);
+    console.log("contentHeight::", contentHeight);
+    console.log("scrollY::", scrollY);
+
+    if(scrollY >= contentHeight - viewportHeight +sidebarTop) {
+      sidebarContent.style.position = 'sticky';
+      sidebarContent.style.top = '2rem';
+      sidebarContent.style.width = `${originalWidth}px`; // Use original width instead of 125%
+      sidebarContent.style.maxWidth = '100%'; // Ensure it doesn't exceed container
+      sidebarContent.style.marginLeft = '0';
+      sidebarContent.style.marginRight = '0';
+      sidebarContent.style.overflowY = 'auto';
+      sidebarContent.style.height = 'fit-content';
+      sidebarContent.style.zIndex = '10';
+      sidebarContent.style.boxSizing = 'border-box';
+    } else {
+      // Reset styles when not sticky
+      sidebarContent.style.position = '';
+      sidebarContent.style.top = '';
+      sidebarContent.style.width = '';
+      sidebarContent.style.zIndex = '';
+      sidebarContent.style.maxHeight = '';
+      sidebarContent.style.overflowY = '';
     }
+  };
+  handleScroll();
 
-    stickySidebarEvents() {
-        const sidebar = document.querySelector('.cmp-article-container__sidebar-wrapper');
-        if (!sidebar) return;
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('resize', handleScroll);
 
-        const handleScroll = () => {
-            const currentTopOffset = this.calculateTopOffset();
-            sidebar.style.top = `${currentTopOffset}px`;
-
-            //const sidebarRect = document.getElementsByClassName('cmp-article-container__sidebar-wrapper')[0];
-            const sidebarContent = document.getElementsByClassName('cmp-experiencefragment--sidebar')[0];
-            const scrollY = window.scrollY;
-            const viewportHeight = window.innerHeight;
-            const contentHeight = sidebarContent.getBoundingClientRect().height;
-            //const sidebarTop = sidebarRect.getBoundingClientRect().top + scrollY;
-
-            if(scrollY >= contentHeight - viewportHeight) {
-                sidebar.style.position = 'sticky';
-                sidebar.style.maxHeight = 'calc(150vh - 4rem)';
-                sidebar.style.overflowY = 'auto';
-                sidebar.style.height = 'fit-content';
-                sidebar.style.zIndex = '10';
-                sidebar.style.transform = `translateY(-${contentHeight - viewportHeight}px)`;
-            }
-        };
-        handleScroll();
-
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('resize', handleScroll);
-
-        this.cleanup = () => {
-            window.removeEventListener('scroll', handleScroll);
-            window.removeEventListener('resize', handleScroll);
-        };
-    }
-
-    calculateTopOffset() {
-        const header = document.querySelector('header, .header, .cmp-header, [class*="header"]');
-        if (header) {
-            const headerHeight = header.offsetHeight;
-            return headerHeight + 16;
-        }
-    }
+  this.cleanup = () => {
+    window.removeEventListener('scroll', handleScroll);
+    window.removeEventListener('resize', handleScroll);
+  };
+}
+*/
 }
 
 export default new StickySidebar();
